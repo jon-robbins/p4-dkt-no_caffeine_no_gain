5fcb0b5cf22663e2593c8c8c7880363aafc40eca
jon-robbins <117274763+jon-robbins@users.noreply.github.com>
2023-06-06 16:57:50 +0200

    resolved junyi preproc conflicts

--- make_custom_data/bse_junyi_preprocessing.py
+++ make_custom_data/bse_junyi_preprocessing.py
@@ -27,10 +27,10 @@ def junyi_preprocessing(n=10_000):
         'level4_id_int':'testId',
         'ucid_int': 'KnowledgeTag',
         'is_correct': 'answerCode',
-        'timestamp_TW': 'Timestamp'
+        'timestamp_TW': 'Timestamp',
+        'total_sec_taken': 'elapsed',
     }
 
-<<<<<<< HEAD
     #create new ID's that are only integers, then merge
     df_problems['upid_int'] = random.sample(range(100_000_000, 200_000_000 + 1), len(df_problems))
     df_content['ucid_int'] = random.sample(range(1_000, 10_000 + 1), len(df_content))
@@ -50,14 +50,3 @@ def junyi_preprocessing(n=10_000):
     df_train, df_test = train_test_split(df_merged, test_size=0.1, random_state=42069)
     print('done with train test split')
     return df_train, df_test
-=======
-    df_problems_merged = pd.merge(df_problems, df_content[['ucid', 'level4_id', 'subject']], on='ucid', how='left')
-    df_problems_merged = df_problems_merged.rename(columns=map_dict)
-    to_drop =  ['problem_number','exercise_problem_repeat_session', 'total_sec_taken','total_attempt_cnt', 'used_hint_cnt', 'is_hint_used', 'is_downgrade','is_upgrade', 'level',]
-    df_problems_merged = df_problems_merged.drop(columns=to_drop)
-    df_problems_merged_sample = df_problems_merged.sample(n=n)
-    df_train, df_test = train_test_split(df_problems_merged_sample, test_size=0.1, random_state=42069)
-
-    df_train.to_csv('../archive/caffeine_data/df_train_make_elapsed.csv', index=False)
-    df_test.to_csv('../archive/caffeine_data/df_test_make_elapsed.csv', index=False)
->>>>>>> def37cdcf7975ca72ee6045aea1bf334c450d538
