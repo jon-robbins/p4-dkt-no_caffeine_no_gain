5f6ea96c7fd0ad6668062e83f120be5310c02b34
jon-robbins <117274763+jon-robbins@users.noreply.github.com>
2023-06-06 16:13:23 +0200

    Update bse_junyi_preprocessing.py

--- make_custom_data/bse_junyi_preprocessing.py
+++ make_custom_data/bse_junyi_preprocessing.py
@@ -30,7 +30,7 @@ def junyi_preprocessing(problems_csv, users_csv, content_csv):
     df_users['uuid_int'] = random.sample(range(50_000, 200_000 + 1), len(df_users))
     df_merged = pd.merge(df_merged, df_content[['ucid', 'ucid_int', 'level4_id_int']], on='ucid', how='left')
     df_merged = pd.merge(df_merged, df_content[['ucid', 'ucid_int', 'level4_id_int']], on='ucid', how='left')
-
+#adding this for testing
     #drop unnecessary features
     to_drop =  ['problem_number','exercise_problem_repeat_session', 'total_attempt_cnt', 'used_hint_cnt', 'is_hint_used', 'is_downgrade','is_upgrade', 'level','uuid', 'ucid', 'upid']    
     print(f"cols before drop: {df_merged.columns}")
